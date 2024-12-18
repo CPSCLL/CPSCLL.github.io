0ae8f5e604a659d0522dc94bb250d41c33f50486
CPSCLL <137359828+CPSCLL@users.noreply.github.com>
2024-12-18 19:26:19 +0800

    Update journal_club.js

--- js/journal_club.js
+++ js/journal_club.js
@@ -316,7 +316,7 @@ async function downloadAllPapers8() {
 
 async function downloadAllPapers9() {
     const zip = new JSZip();
-    const folder = zip.folder("Papers_JinghaoLiu_20241226"); // 创建一个文件夹来存储所有论文
+    const folder = zip.folder("Papers_JinghaoLiu_20241225"); // 创建一个文件夹来存储所有论文
 
     // 定义所有文件的路径
     const files = [
@@ -340,7 +340,7 @@ async function downloadAllPapers9() {
 
         // 生成ZIP文件并触发下载
         zip.generateAsync({ type: 'blob' }).then(function(content) {
-            saveAs(content, "Papers_JinghaoLiu_20241226.zip"); // 使用FileSaver.js保存文件
+            saveAs(content, "Papers_JinghaoLiu_20241225.zip"); // 使用FileSaver.js保存文件
         });
     } catch (error) {
         console.error('Error downloading files:', error);
